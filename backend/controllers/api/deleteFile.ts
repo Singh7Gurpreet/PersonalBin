5be6969c666cbf22380ee31174806ec814f63d61
Gurpreet Singh <parasrockz81@gmail.com>
2025-05-21 14:03:15 -0600

    Minor changes

--- backend/controllers/api/deleteFile.ts
+++ backend/controllers/api/deleteFile.ts
@@ -5,6 +5,7 @@ const deleteFileS3 = async (req: Request, res: Response) => {
   try{
     const { email }  = req.user as {email:string};
     await awsDeleteFile(email);
+    res.status(200).json({message : "OK"});
   } catch (error) {
     res.status(500).json({message : error});
   }
