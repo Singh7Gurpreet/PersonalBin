14b95d750f9b1377aff4298fe8ec63dc76cb9b58
Gurpreet Singh <parasrockz81@gmail.com>
2025-06-24 13:21:20 -0600

    Moved redis port too into environment

--- backend/lib/singletonRedisClient.ts
+++ backend/lib/singletonRedisClient.ts
@@ -7,9 +7,10 @@ const client = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
-        port: 12264
+        port: Number(process.env.REDIS_PORT)
     }
 });
+
 
 client.on('error', err => console.log('Redis Client Error', err));
 
